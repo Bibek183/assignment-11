8df0d262badaf84722c0306a98342503cb988d71
Forhad427 <you@example.com>
2025-08-17 02:28:34 +0100

    comments removed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-    // Menu toggle for mobile
+ 
     $("#menu").click(function(){
         $("header .navbar").toggleClass("nav-toggle");
     });
 
-    // Scroll to top button visibility
     $(window).scroll(function(){
         if($(this).scrollTop() > 300){
             $(".scroll-top").fadeIn();
@@ -13,7 +12,6 @@ $(document).ready(function(){
         }
     });
 
-    // Smooth scroll for scroll-top button
     $(".scroll-top").click(function(e){
         e.preventDefault();
         $("html, body").animate({scrollTop:0}, 800);
